e86c2a4d03b261e99437bbf571d7401b6d04d708
M4TT0CK <jamesnthomas42@gmail.com>
2019-08-24 10:48:28 -0500

    Adds comments to problems 1 and 2

--- Problem-1.py
+++ Problem-1.py
@@ -1,5 +1,8 @@
-sum = 0
+# Problem 1: Find the sum of all the multiples of 3 or 5 below 1000
 
+sum = 0 # Rolling sum
+
+# Iterates x...999; if x is divisible by 3 or 5, x is added to rolling sum
 for x in range(1000):
     if (x % 3 == 0) or (x % 5 == 0):
         sum += x
--- Problem-2.py
+++ Problem-2.py
@@ -1,13 +1,17 @@
-sum = 0
+# Problem 2: By considering the terms in the Fibonacci sequence whose 
+# values do not exceed four million, find the sum of the even-valued terms
+
+sum = 0 # Rolling sum
 phi = (1 + (5**.5)) / 2
 psi = (1 - (5**.5)) / 2
-fib = 0
-x = 0
+fib = 0 # Placeholder variable for the given Fibonacci number
+x = 0 # Rolling x value
 
 while fib <= 4000000:
+    # Calculuates Fibonacci number for a given x using Binet's formula
     fib = int((phi**x - psi**x) / (5**.5))
-    if (fib % 2) == 0:
-        sum += fib
-    x += 1
+    if (fib % 2) == 0: # Checks for evenness
+        sum += fib # Adds to rolling sum
+    x += 1 # Iterates x
 
 print(sum)
