cccb7bf02ac82477be8052ad1debf38bf10a78cf
James <Jamesnthomas42@gmail.com>
2020-07-14 14:44:52 -0500

    Adds optimized iterative solution

--- Problem-7.py
+++ Problem-7.py
@@ -2,24 +2,18 @@ import math
 import Project_Euler
 # Problem 7: What is the 10001st prime number?
 
-# Enlarges the number to run through the sieve by a scale factor of 50 with each
-# iteration. Once the length of the array returned outstrips tne index needed,
-# the array index is called, thus providing the solution.
+# Uses a simple primality function and iterates up to the desired index.
+# Least expensive solution so far.
 def nthPrime(n):
-    p = []
-    index = 1
+    p = [2]
+    index = 3
     length = 0
-    last = 0
 
     while length != n:
         length = len(p)
-        if length < n:
-            last = index
-            index = index * 50
-            p = Project_Euler.sieveOfEratosthenes(index)
-        if length > n:
-            return p[n - 1]
-        print(length)
+        if Project_Euler.isPrime(index):
+            p.append(index)
+        index += 2
     
     return p[n - 1]
 
