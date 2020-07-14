3aa0cde9244191f0dc54a3e45f3c261fc95dd8db
James <Jamesnthomas42@gmail.com>
2020-07-14 14:44:42 -0500

    Adds primality checker

--- Project_Euler.py
+++ Project_Euler.py
@@ -21,3 +21,25 @@ def sieveOfEratosthenes(n):
         h += 1
     
     return primes
+
+# Source: https://www.w3schools.com/python/python_for_loops.asp
+def isPrime(n) : 
+  
+    # Corner cases 
+    if (n <= 1) : 
+        return False
+    if (n <= 3) : 
+        return True
+  
+    # This is checked so that we can skip  
+    # middle five numbers in below loop 
+    if (n % 2 == 0 or n % 3 == 0) : 
+        return False
+  
+    i = 5
+    while(i * i <= n) : 
+        if (n % i == 0 or n % (i + 2) == 0) : 
+            return False
+        i = i + 6
+  
+    return True
